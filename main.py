33fb2b8727a014a337ab3521b5c1dcbfbd1770d3
Spencer McWilliams <spencer.mcw@gmail.com>
2021-03-12 16:36:22 -0800

    Removes VSCode; Adds Pass/Salt confirm

--- main.py
+++ main.py
@@ -19,40 +19,66 @@ def pass_to_key(password, salt):
   key = base64.urlsafe_b64encode(kdf.derive(password))
   return key
 
-def main():
-  # Prompt
-  print('Password = Written Down.')
-  print('Salt = Not Written Down.\n')
-  # Build Fernet
-  password = getpass('Password> ')
-  salt = getpass('Salt> ')
-  key = pass_to_key(password, salt)
-  f = Fernet(key)
-  # Determine Operation
-  operation = input('[1] Encrypt\n[2] Decrypt\n> ')
-  if operation not in ['1','2']:
+def get_pass():
+  password = getpass('Enter Password> ')
+  password_conf = getpass('Confirm Password> ')
+  if password != password_conf:
+    print("Passwords Did Not Match")
     exit()
-  # Handle Files
-  ifile = input('Input File> ').strip('"')
-  ofile = input('Ouptput File> ').strip('"')
+  return password
+
+def get_salt():
+  salt = getpass('Enter Salt> ')
+  salt_conf = getpass('Confirm Salt> ')
+  if salt != salt_conf:
+    print("Salts Did Not Match")
+    exit()
+  return salt
+
+def get_operation():
+  print('[1] Encrypt\n[2] Decrypt')
+  operation = input('Desired Operation> ')
+  if operation not in ['1','2']:
+    print('Invalid Operation')
+    exit()
+  return operation
+
+def get_files():
+  ifile = input('Input File Path> ').strip('"')
+  ofile = input('Ouptput File Path> ').strip('"')
   try:
     ifile = open(ifile, 'rb')
     ofile = open(ofile, 'wb')
   except OSError:
-    print('Could not open one or both of the specified files')
+    print('Could not open one or both of the specified file paths.')
     exit()
-  # Do Work
-  with ifile, ofile:
-    # Encrypt
-    if operation == '1':
-      token = f.encrypt(ifile.read())
-    # Decrypt
-    else:
-      token = f.decrypt(ifile.read())
-    # Write
-    ofile.write(token)
-    # Close Files
-    ifile.close()
-    ofile.close()
+  return (ifile, ofile)
+
+def do_work(ifile, ofile, operation, f):
+  # Encrypt
+  if operation == '1':
+    token = f.encrypt(ifile.read())
+  # Decrypt
+  else:
+    token = f.decrypt(ifile.read())
+  # Write
+  ofile.write(token)
+  # Close Files
+  ifile.close()
+  ofile.close()
+
+def main():
+  # Prompts
+  print('Password = Written Down.')
+  print('Salt = Not Written Down.')
+  # Build Fernet
+  password = get_pass()
+  salt = get_salt()
+  key = pass_to_key(password, salt)
+  f = Fernet(key)
+  # Handle Operation
+  operation = get_operation()
+  ifile, ofile = get_files()
+  do_work(ifile, ofile, operation, f)
 
 main()
